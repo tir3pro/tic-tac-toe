e3a573a720e4332d39b692eb6de60c7e3b31a278
Yevhen Kryvsun <tir3pro@gmail.com>
2017-03-31 11:23:43 +0300

    Removed calculateWinner function import

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Game from './components/Game';
-import { calculateWinner } from './helpers/calculateWinner';
 import './styles.scss';
 
 ReactDOM.render(<Game />, document.querySelector('#root'));
